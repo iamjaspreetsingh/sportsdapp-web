50cd7e594f0f0447cf24759684c51e5c1b17ecc5
iamjaspreetsingh <jsk1961998@gmail.com>
2020-08-24 00:28:50 +0530

    first commit

---
+++ src/components/layout/partials/FooterNav.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import classNames from 'classnames';
+import { Link } from 'react-router-dom';
+
+const FooterNav = ({
+  className,
+  ...props
+}) => {
+
+  const classes = classNames(
+    'footer-nav',
+    className
+  );
+
+  return (
+    <nav
+      {...props}
+      className={classes}
+    >
+      <ul className="list-reset">
+        <li>
+          <a href="https://mail.google.com/mail/?view=cm&fs=1&to=info.sportsdapp@gmail.com" target="_blank">Contact us</a>
+        </li>
+        {/* <li>
+          <Link to="#0">About us</Link>
+        </li>
+        <li>
+          <Link to="#0">FAQ's</Link>
+        </li>
+        <li>
+          <Link to="#0">Support</Link>
+        </li> */}
+      </ul>
+    </nav>
+  );
+}
+
+export default FooterNav;
---
+++ src/components/sections/FeaturesTiles.js
@@ -0,0 +1,203 @@
+import React from 'react';
+import classNames from 'classnames';
+import { SectionTilesProps } from '../../utils/SectionProps';
+import SectionHeader from './partials/SectionHeader';
+import Image from '../elements/Image';
+
+const propTypes = {
+  ...SectionTilesProps.types
+}
+
+const defaultProps = {
+  ...SectionTilesProps.defaults
+}
+const FeaturesTiles = ({
+  className,
+  topOuterDivider,
+  bottomOuterDivider,
+  topDivider,
+  bottomDivider,
+  hasBgColor,
+  invertColor,
+  pushLeft,
+  ...props
+}) => {
+
+  const outerClasses = classNames(
+    'features-tiles section',
+    topOuterDivider && 'has-top-divider',
+    bottomOuterDivider && 'has-bottom-divider',
+    hasBgColor && 'has-bg-color',
+    invertColor && 'invert-color',
+    className
+  );
+
+  const innerClasses = classNames(
+    'features-tiles-inner section-inner pt-0',
+    topDivider && 'has-top-divider',
+    bottomDivider && 'has-bottom-divider'
+  );
+
+  const tilesClasses = classNames(
+    'tiles-wrap center-content',
+    pushLeft && 'push-left'
+  );
+
+  const sectionHeader = {
+    title: 'Embrace your game knowledge ',
+    paragraph: 'Ask your Questions, Answer what you know Best & Win Rewards'
+  };
+
+  return (
+    <section
+      {...props}
+      className={outerClasses}
+    >
+      <div className="container">
+        <div className={innerClasses}>
+          <SectionHeader data={sectionHeader} className="center-content" />
+          <div className={tilesClasses}>
+
+            <div className="tiles-item reveal-from-bottom">
+              <div className="tiles-item-inner">
+                <div className="features-tiles-item-header">
+                  <div className="features-tiles-item-image mb-16">
+                    <Image
+                      src={require('./../../assets/images/feature-tile-icon-01.svg')}
+                      alt="Features tile icon 01"
+                      width={64}
+                      height={64} />
+                  </div>
+                </div>
+                <div className="features-tiles-item-content">
+                  <h4 className="mt-0 mb-8">
+                    Your Game Your Rules
+                    </h4>
+                  <p className="m-0 text-sm">
+                  Users create the questions and decide the rewards to the winners
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="tiles-item reveal-from-bottom" data-reveal-delay="200">
+              <div className="tiles-item-inner">
+                <div className="features-tiles-item-header">
+                  <div className="features-tiles-item-image mb-16">
+                    <Image
+                      src={require('./../../assets/images/feature-tile-icon-02.svg')}
+                      alt="Features tile icon 02"
+                      width={64}
+                      height={64} />
+                  </div>
+                </div>
+                <div className="features-tiles-item-content">
+                  <h4 className="mt-0 mb-8">
+                  Transparent Verifiable rewards
+                    </h4>
+                  <p className="m-0 text-sm">
+                  Fair chance for everyone to win with blockchain powered distribution of funds
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="tiles-item reveal-from-bottom" data-reveal-delay="400">
+              <div className="tiles-item-inner">
+                <div className="features-tiles-item-header">
+                  <div className="features-tiles-item-image mb-16">
+                    <Image
+                      src={require('./../../assets/images/feature-tile-icon-03.svg')}
+                      alt="Features tile icon 03"
+                      width={64}
+                      height={64} />
+                  </div>
+                </div>
+                <div className="features-tiles-item-content">
+                  <h4 className="mt-0 mb-8">
+                    Artificial Intelligence
+                    </h4>
+                  <p className="m-0 text-sm">
+                    Our cutting edge NLP techniques allow users to ask any relevant game question & automatically listen to the result based on game events
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="tiles-item reveal-from-bottom">
+              <div className="tiles-item-inner">
+                <div className="features-tiles-item-header">
+                  <div className="features-tiles-item-image mb-16">
+                    <Image
+                      src={require('./../../assets/images/feature-tile-icon-04.svg')}
+                      alt="Features tile icon 04"
+                      width={64}
+                      height={64} />
+                  </div>
+                </div>
+                <div className="features-tiles-item-content">
+                  <h4 className="mt-0 mb-8">
+                    Multiplayer Games
+                    </h4>
+                  <p className="m-0 text-sm">
+                    Many Multiplayer Games like Math Clash, Cricket Card game, Speed Chess etc. are available for playing alongwith Sports fantasy
+                    </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="tiles-item reveal-from-bottom" data-reveal-delay="200">
+              <div className="tiles-item-inner">
+                <div className="features-tiles-item-header">
+                  <div className="features-tiles-item-image mb-16">
+                    <Image
+                      src={require('./../../assets/images/feature-tile-icon-05.svg')}
+                      alt="Features tile icon 05"
+                      width={64}
+                      height={64} />
+                  </div>
+                </div>
+                <div className="features-tiles-item-content">
+                  <h4 className="mt-0 mb-8">
+                    Global competiton
+                    </h4>
+                  <p className="m-0 text-sm">
+                    Participation through cryptocurrencies allow users to participate from anywhere in the world with no worry of conversions & faster checkouts
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="tiles-item reveal-from-bottom" data-reveal-delay="400">
+              <div className="tiles-item-inner">
+                <div className="features-tiles-item-header">
+                  <div className="features-tiles-item-image mb-16">
+                    <Image
+                      src={require('./../../assets/images/feature-tile-icon-06.svg')}
+                      alt="Features tile icon 06"
+                      width={64}
+                      height={64} />
+                  </div>
+                </div>
+                <div className="features-tiles-item-content">
+                  <h4 className="mt-0 mb-8">
+                    Easy to Choose
+                    </h4>
+                  <p className="m-0 text-sm">
+                    We provide the MCQ based questions that also feed the live trends of the participation allowing user to choose between Risk & Gain
+                    </p>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+FeaturesTiles.propTypes = propTypes;
+FeaturesTiles.defaultProps = defaultProps;
+
+export default FeaturesTiles;
